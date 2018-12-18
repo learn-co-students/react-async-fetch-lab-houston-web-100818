2098ae2df78760f19c4c6ecc61b609afad29726c
barbaradecares <barbaradecares@hotmail.com>
2018-12-18 11:42:36 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
-// create your App component here
+import React, { Component } from "react";
+import Greeting from "./components/Greeting";
+
+export default class App extends Component {
+  fetchData() {
+    fetch("http://api.open-notify.org/astros.json").then(response => {
+      return response.json();
+    });
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+  render() {
+    return <Greeting />;
+  }
+}
